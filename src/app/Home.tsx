eadddfd600351a318c500113c1b5dd2bca5f2cf4
Luis Siliezar <lsiliezarjr@gmail.com>
2025-02-04 20:54:55 -0600

    feat: added global colors and initial styles for pages

---
+++ src/app/Home.tsx
@@ -0,0 +1,129 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { toast as sonner } from "sonner";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useToast } from "@/hooks/use-toast";
+import { ToastAction } from "@/components/ui/toast";
+import { Toaster as Sonner } from "sonner";
+import { Toaster } from "@/components/ui/toaster";
+
+export default function Home() {
+    const { toast } = useToast();
+
+    return (
+        <div className="grid items-center justify-items-center min-h-screen p-4 pb-20 gap-8 sm:p-8 md:p-12 lg:p-20 font-[family-name:var(--font-geist-sans)]">
+            <Sonner />
+            <Toaster />
+            <main className="flex flex-col gap-8 w-full max-w-4xl">
+                <Button onClick={() => alert("Hello")} className="w-full sm:w-auto">
+                    Click me
+                </Button>
+
+                <Accordion type="single" collapsible className="w-full">
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-2">
+                        <AccordionTrigger>Is it styled?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It comes with default styles that matches the other
+                            components&apos; aesthetic.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-3">
+                        <AccordionTrigger>Is it animated?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. Its animated by default, but you can disable it if you prefer.
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
+
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button variant="outline" className="w-full sm:w-auto">
+                            Edit Profile
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                            <DialogTitle>Edit profile</DialogTitle>
+                            <DialogDescription>
+                                Make changes to your profile here. Click save when you&apos;re done.
+                            </DialogDescription>
+                        </DialogHeader>
+                        <div className="grid gap-4 py-4">
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label htmlFor="name" className="text-right">
+                                    Name
+                                </Label>
+                                <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                            </div>
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label htmlFor="username" className="text-right">
+                                    Username
+                                </Label>
+                                <Input id="username" value="@peduarte" className="col-span-3" />
+                            </div>
+                        </div>
+                        <DialogFooter>
+                            <Button type="submit">Save changes</Button>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
+                {/* fix: Labels */}
+                <Label>Default</Label>
+                <Button
+                    variant="outline"
+                    onClick={() => {
+                        toast({
+                            title: "Scheduled: Catch up ",
+                            description: "Friday, February 10, 2023 at 5:57 PM",
+                            action: (
+                                <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
+                            ),
+                        });
+                    }}
+                    className="w-full sm:w-auto"
+                >
+                    Add to calendar
+                </Button>
+
+                <Button
+                    variant="action"
+                    onClick={() =>
+                        sonner("Event has been created", {
+                            description: "Sunday, December 03, 2023 at 9:00 AM",
+                            action: {
+                                label: "Undo",
+                                onClick: () => console.log("Undo"),
+                            },
+                        })
+                    }
+                    className="w-full sm:w-auto"
+                >
+                    Show sonner
+                </Button>
+            </main>
+        </div>
+    );
+}
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,26 +5,32 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-base font-medium ring-offset-background transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-turquoise text-black font-bold hover:bg-turquoise/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input text-primary-foreground hover:bg-charcoal-night",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        secondarySelected:
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80 outline outline-offset-4 outline-sky-blue",
+        ghost: "text-white/50 hover:bg-accent hover:text-white/30",
+        link: "text-white underline-offset-4 hover:underline",
+        disabled: "bg-stone-dark-gray text-white/50 cursor-not-allowed",
+        action: "bg-stone-light-gray text-white hover:bg-stone-light-gray/50",
+        actionSelected: "bg-stone-light-gray text-white hover:bg-stone-light-gray/50 outline outline-offset-4 outline-sky-blue",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
-      },
+        default: "h-12 px-4 py-2",
+        sm: "h-9 rounded-full px-3 text-sm",
+        lg: "h-11 rounded-full px-8",
+        icon: "h-12 w-12",
+        full: "h-12 w-full",
+      }
     },
     defaultVariants: {
       variant: "default",
@@ -35,7 +41,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
