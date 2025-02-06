527f5b11d5058a43127039987e73dddf0eed7155
Luis Siliezar <lsiliezarjr@gmail.com>
2025-02-06 15:46:46 -0600

    feat: added form container

--- src/app/dashboard/Dashboard.tsx
+++ src/app/dashboard/Dashboard.tsx
@@ -1,12 +1,173 @@
+import FormContainer from '@/components/containers/FormContainer';
+import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
 import React from 'react';
 
 const Dashboard = () => {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <h1 className="text-3xl font-bold">Dashboard</h1>
-            </main>
-        </div>
+        <FormContainer
+            navbar={
+                <div className="flex mx-auto flex-col gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            NAVBAR
+                        </Label>
+                    </div>
+                </div>
+            }
+            container={
+                <div className="">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Name
+                        </Label>
+                    </div>
+                    {/* <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Username
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Name
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Username
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Name
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Username
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Name
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Username
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Name
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Username
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Name
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Username
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Name
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Username
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Name
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Name
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Username
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Name
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Username
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Name
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Username
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Name
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Username
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Name
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Username
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Name
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Username
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Name
+                        </Label>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className='text-white'>
+                            Username
+                        </Label>
+                    </div> */}
+                </div>
+            }
+            button={<Button size='full'>Edit Profile</Button>}
+
+
+        />
     );
 }
 
